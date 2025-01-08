3f918ad235efb54d74282b0dd609ffd6a9dfd6d6
Lou Andria <lou@hei.school>
2025-01-08 22:14:07 +0100

    fix: set all dummy env for IT tests

--- src/test/java/school/hei/asa/conf/EnvConf.java
+++ src/test/java/school/hei/asa/conf/EnvConf.java
@@ -8,5 +8,17 @@ public class EnvConf {
 
   void configureProperties(DynamicPropertyRegistry registry) {
     registry.add("asa.care.product.code", () -> DUMMY_CARE_PRODUCT_CODE);
+    registry.add("spring.security.oauth2.client.provider.cognito.authorization-uri", () -> "dummy");
+    registry.add("spring.security.oauth2.client.provider.cognito.token-uri", () -> "dummy");
+    registry.add("spring.security.oauth2.client.registration.cognito.provider", () -> "cognito");
+    registry.add("spring.security.oauth2.client.registration.cognito.client-id", () -> "dummy");
+    registry.add(
+        "spring.security.oauth2.client.registration.cognito.redirect-uri",
+        () -> "{baseUrl}/login/oauth2/code/cognito");
+    registry.add(
+        "spring.security.oauth2.client.registration.cognito.authorization-grant-type",
+        () -> "authorization_code");
+    registry.add("cognito.logout.url", () -> "dummy");
+    registry.add("asa.logout.url", () -> "dummy");
   }
 }

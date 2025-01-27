c881606b2a7d8aaf6a94029bdaab56e811c55b74
lianafinaritra <hei.lianafinaritra@gmail.com>
2025-01-27 23:22:28 +0300

    feat: sort missions executions per month

--- src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
@@ -1,12 +1,15 @@
 package school.hei.asa.endpoint.rest.controller;
 
-import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -46,19 +49,35 @@ public class MissionController {
 
   @GetMapping("/mission-executions")
   public String getMissionExecutions(
-      Model model, @RequestParam(required = false) String workerCode) {
-    var dailyExecutionsByDate = dailyExecutionsByDate(workerCode);
+          Model model,
+          @RequestParam(required = false) String workerCode,
+          @RequestParam(required = false) String month) {
+
+    YearMonth currentMonth = (month == null || month.isBlank())
+            ? YearMonth.now()
+            : YearMonth.parse(month);
+
+    var dailyExecutionsByDate = dailyExecutionsByDate(workerCode)
+            .entrySet()
+            .stream()
+            .filter(entry -> YearMonth.from(entry.getKey()).equals(currentMonth))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
     var thDailyExecutions = new ArrayList<ThDailyExecution>();
     dailyExecutionsByDate.forEach(
-        (date, deList) -> thDailyExecutions.add(thDailyExecutionMapper.toTh(date, deList)));
-    model.addAttribute(
-        "dailyExecutions",
-        thDailyExecutions.stream().sorted(comparing(ThDailyExecution::date).reversed()).toList());
-    model.addAttribute("careProductCode", careProductCodeSupplier.get());
+            (date, deList) -> thDailyExecutions.add(thDailyExecutionMapper.toTh(date, deList)));
 
+    model.addAttribute("dailyExecutions",
+            thDailyExecutions.stream()
+                    .sorted(Comparator.comparing(ThDailyExecution::date).reversed())
+                    .toList());
+    model.addAttribute("careProductCode", careProductCodeSupplier.get());
+    model.addAttribute("currentMonth", currentMonth.toString());
     workerToModelAdder.apply(workerCode, model);
+
     return "mission-executions";
   }
+
 
   private Map<LocalDate, List<DailyExecution>> dailyExecutionsByDate(String workerCode) {
     List<DailyExecution> allDailyExecutions = dailyExecutionRepository.findAll();

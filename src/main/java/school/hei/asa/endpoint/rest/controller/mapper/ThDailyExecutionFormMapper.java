48aa287d92745ddb233c2fa462a2f326f96baa15
lianafinaritra <hei.lianafinaritra@gmail.com>
2025-04-10 15:41:03 +0300

    fix: missing date error message

--- src/main/java/school/hei/asa/endpoint/rest/controller/mapper/ThDailyExecutionFormMapper.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/mapper/ThDailyExecutionFormMapper.java
@@ -36,8 +36,12 @@ public class ThDailyExecutionFormMapper {
     var percentage5Opt = optionalPercentage(dmeForm.missionPercentage5());
 
     List<MissionExecution> executions = new ArrayList<>();
+    if (dmeForm.date() == null || dmeForm.date().isBlank()){
+      throw new IllegalArgumentException("Please enter a date");
+    }
     var date = LocalDate.parse(dmeForm.date());
     var creationInstant = Instant.now();
+
     optionalAdd(
         executions,
         worker,

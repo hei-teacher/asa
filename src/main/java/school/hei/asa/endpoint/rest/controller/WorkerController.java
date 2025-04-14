46b1c228279a02021d2d6a7d140abc8dd07e8d4f
lianafinaritra <hei.lianafinaritra@gmail.com>
2025-04-14 15:00:03 +0300

    feat: add worker informations in profile page

--- src/main/java/school/hei/asa/endpoint/rest/controller/WorkerController.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/WorkerController.java
@@ -2,8 +2,12 @@ package school.hei.asa.endpoint.rest.controller;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import school.hei.asa.endpoint.rest.security.WorkerFromAuthentication;
 import school.hei.asa.model.Worker;
 import school.hei.asa.repository.WorkerRepository;
 
@@ -12,6 +16,7 @@ import school.hei.asa.repository.WorkerRepository;
 public class WorkerController {
 
   private final WorkerRepository workerRepository;
+  private final WorkerFromAuthentication workerFromAuthentication;
 
   @GetMapping("/workers")
   public List<Worker> getWorkers() {
@@ -19,7 +24,17 @@ public class WorkerController {
   }
 
   @GetMapping("/worker")
-  public String getWorker() {
+  public String getWorker(Model model,
+                          Authentication authentication,
+                          @RequestParam(required = false) String workerCode) {
+    var workerCodeOrAuth =
+            workerCode == null || workerCode.isBlank()
+                    ? workerFromAuthentication.apply(authentication).get().code()
+                    : workerCode;
+
+    Worker worker = workerRepository.findByCode(workerCodeOrAuth);
+
+    model.addAttribute("worker", worker);
     return "worker";
   }
 }

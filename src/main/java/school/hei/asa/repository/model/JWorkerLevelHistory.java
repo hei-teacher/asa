326e179241b791ec5f73f529e87563892e9eb47e
lianafinaritra <hei.lianafinaritra@gmail.com>
2025-04-28 16:16:37 +0300

    fix: remove worker level enum test

--- src/main/java/school/hei/asa/repository/model/JWorkerLevelHistory.java
+++ src/main/java/school/hei/asa/repository/model/JWorkerLevelHistory.java
@@ -1,12 +1,9 @@
 package school.hei.asa.repository.model;
 
-import static org.hibernate.type.SqlTypes.NAMED_ENUM;
-
 import jakarta.persistence.*;
 import java.time.Instant;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.JdbcTypeCode;
 
 @Entity
 @Table(name = "worker_level_history")
@@ -22,7 +19,6 @@ public class JWorkerLevelHistory {
   @JoinColumn(name = "worker_code")
   private JWorker worker;
 
-  @JdbcTypeCode(NAMED_ENUM)
   @Column(nullable = false)
   private String level;
 

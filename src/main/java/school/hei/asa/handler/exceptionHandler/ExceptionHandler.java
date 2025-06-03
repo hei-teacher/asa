48b2ca90c9941ec3c34d1f08c3914acdb7456240
Mahefaa <hei.mahefa@gmail.com>
2025-06-03 10:58:44 +0300

    feat: new LambdaHandler impl, and rm unused files

--- src/main/java/school/hei/asa/handler/exceptionHandler/ExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package school.hei.asa.handler.exceptionHandler;
-
-import school.hei.asa.PojaGenerated;
-
-@PojaGenerated
-public interface ExceptionHandler<R> {
-  R handle(Throwable throwable);
-}
--- src/main/java/school/hei/asa/handler/model/ErrorModel.java
+++
@@ -1,7 +0,0 @@
-package school.hei.asa.handler.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import school.hei.asa.PojaGenerated;
-
-@PojaGenerated
-public record ErrorModel(@JsonProperty("message") String message) {}

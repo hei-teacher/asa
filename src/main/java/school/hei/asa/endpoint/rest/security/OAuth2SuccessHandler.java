5afd75206af1df39909b39c33cb452c2c2ad3529
Lou Andria <lou@hei.school>
2025-06-30 08:29:57 +0300

    fix: redirect to work-and-care-calendar onAuthenticationSuccess

--- src/main/java/school/hei/asa/endpoint/rest/security/OAuth2SuccessHandler.java
+++ src/main/java/school/hei/asa/endpoint/rest/security/OAuth2SuccessHandler.java
@@ -1,25 +1,24 @@
 package school.hei.asa.endpoint.rest.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Component
-public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
   private final WorkerFromAuthentication workerFromAuthentication;
 
   @Override
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-      throws IOException, ServletException {
+      throws IOException {
     var workerOpt = workerFromAuthentication.apply(authentication);
     if (workerOpt.isEmpty()) {
       var principal = (DefaultOidcUser) authentication.getPrincipal();
@@ -27,6 +26,6 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
           "Email does not correspond to a known worker: " + principal.getEmail());
     }
 
-    super.onAuthenticationSuccess(request, response, authentication);
+    response.sendRedirect("/work-and-care-calendar");
   }
 }

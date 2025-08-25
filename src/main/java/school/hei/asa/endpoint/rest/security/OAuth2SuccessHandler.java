b9f5721e3c04bef9984ccfd786ba058dd40aa751
Lova Fenohasina Lala RAFANOMEZANTSOA <44872158+fenohasinalala@users.noreply.github.com>
2025-08-25 14:46:24 +0300

    feat: Casdoor Oauth2 integration

--- src/main/java/school/hei/asa/endpoint/rest/security/OAuth2SuccessHandler.java
+++ src/main/java/school/hei/asa/endpoint/rest/security/OAuth2SuccessHandler.java
@@ -4,6 +4,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
@@ -20,13 +23,25 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
       throws IOException, ServletException {
+    var principal = (DefaultOidcUser) authentication.getPrincipal();
     var workerOpt = workerFromAuthentication.apply(authentication);
     if (workerOpt.isEmpty()) {
-      var principal = (DefaultOidcUser) authentication.getPrincipal();
       throw new RuntimeException(
           "Email does not correspond to a known worker: " + principal.getEmail());
     }
 
+    var roles = principal.getAttributes().get("roles");
+    boolean hasRole =
+        roles instanceof List<?> list
+            && list.stream()
+                .filter(item -> item instanceof Map<?, ?> map && map.get("name") instanceof String)
+                .map(item -> (Map<?, ?>) item)
+                .map(map -> ((String) map.get("name")).toLowerCase(Locale.ROOT))
+                .anyMatch("org_collaborator"::equals);
+    if (!hasRole) {
+      throw new RuntimeException(
+          "User doesn't have correct roles: " + principal.getAttribute("email"));
+    }
     super.onAuthenticationSuccess(request, response, authentication);
   }
 }

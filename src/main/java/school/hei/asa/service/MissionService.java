d9180606f959c0c70aa1238d097fd8a0ca3f0af6
Naitive13 <hei.titan.2@gmail.com>
2025-09-26 16:14:48 +0300

    feat: add button to export worker level history to csv

--- src/main/java/school/hei/asa/service/MissionService.java
+++ src/main/java/school/hei/asa/service/MissionService.java
@@ -1,5 +1,6 @@
 package school.hei.asa.service;
 
+import static java.util.Comparator.comparing;
 import static java.util.Comparator.naturalOrder;
 
 import java.time.LocalDate;
@@ -10,10 +11,15 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.hei.asa.endpoint.rest.controller.mapper.ThProductMapper;
+import school.hei.asa.endpoint.rest.controller.mapper.ThWorkerMapper;
 import school.hei.asa.endpoint.rest.model.th.ThMission;
 import school.hei.asa.endpoint.rest.model.th.ThMissionExecution;
 import school.hei.asa.endpoint.rest.model.th.ThProduct;
+import school.hei.asa.endpoint.rest.model.th.ThWorkerLevelHistory;
+import school.hei.asa.model.Worker;
 import school.hei.asa.repository.ProductRepository;
+import school.hei.asa.repository.WorkerLevelHistoryRepository;
+import school.hei.asa.repository.WorkerRepository;
 
 @Slf4j
 @AllArgsConstructor
@@ -22,16 +28,19 @@ public class MissionService {
 
   private final ProductRepository productRepository;
   private final ThProductMapper thProductMapper;
+  private final WorkerRepository workerRepository;
+  private final WorkerLevelHistoryRepository workerLevelHistoryRepository;
+  private final ThWorkerMapper thWorkerMapper;
 
   private List<ThProduct> filterThProductsByWorkerCode(String workerCode) {
     var thProducts = thProductMapper.toTh(productRepository.findAll());
     return workerCode == null || workerCode.isBlank()
         ? thProducts.stream()
-            .sorted(Comparator.comparing(ThProduct::executedDays, naturalOrder()).reversed())
+            .sorted(comparing(ThProduct::executedDays, naturalOrder()).reversed())
             .toList()
         : thProducts.stream()
             .map(p -> p.filterByWorkerCode(workerCode))
-            .sorted(Comparator.comparing(ThProduct::executedDays, naturalOrder()).reversed())
+            .sorted(comparing(ThProduct::executedDays, naturalOrder()).reversed())
             .toList();
   }
 
@@ -85,7 +94,7 @@ public class MissionService {
     }
     return result.stream()
         .map(p -> p.filterByWorkerCode(workerCode))
-        .sorted(Comparator.comparing(ThProduct::executedDays, naturalOrder()).reversed())
+        .sorted(comparing(ThProduct::executedDays, naturalOrder()).reversed())
         .toList();
   }
 
@@ -139,15 +148,22 @@ public class MissionService {
         .sum();
   }
 
-  public List<ThMission> filterThMissionsByWorkerCode(List<ThProduct> thProducts) {
+  /*
+   * this will consider each mission as a unique mission
+   * */
+  public List<ThMission> thMissionsPerProducts(List<ThProduct> thProducts) {
     List<ThMission> missions = new ArrayList<>();
     thProducts.forEach(p -> missions.addAll(p.missions()));
     return missions.stream()
-        .sorted(Comparator.comparing(ThMission::executedDays, naturalOrder()).reversed())
+        .sorted(comparing(ThMission::executedDays, naturalOrder()).reversed())
         .toList();
   }
 
-  public List<ThMission> thMissionsByWorkerCode(List<ThProduct> thProducts) {
+  /*
+   * this will group missions with the same title
+   * and consider them as one mission
+   * */
+  public List<ThMission> thMissionsFromProducts(List<ThProduct> thProducts) {
     List<ThMission> missions = new ArrayList<>();
 
     thProducts.forEach(
@@ -183,7 +199,28 @@ public class MissionService {
                   });
         });
     return missions.stream()
-        .sorted(Comparator.comparing(ThMission::executedDays, naturalOrder()).reversed())
+        .sorted(comparing(ThMission::executedDays, naturalOrder()).reversed())
         .toList();
+  }
+
+  public Map<Worker, List<ThWorkerLevelHistory>> totalWorkDaysPerWorker(String workerCode) {
+
+    Map<Worker, List<ThWorkerLevelHistory>> result = new HashMap<>();
+    if (workerCode == null || workerCode.isBlank()) {
+      var workers = workerRepository.findAll().stream().sorted(comparing(Worker::name)).toList();
+      workers.parallelStream()
+          .forEach(
+              worker -> {
+                var workerLevelHistories =
+                    thWorkerMapper.toTh(workerLevelHistoryRepository.findAllByWorker(worker));
+                result.put(worker, workerLevelHistories);
+              });
+    } else {
+      var worker = workerRepository.findByCode(workerCode);
+      var workerLevelHistories =
+          thWorkerMapper.toTh(workerLevelHistoryRepository.findAllByWorker(worker));
+      result.put(worker, workerLevelHistories);
+    }
+    return result;
   }
 }

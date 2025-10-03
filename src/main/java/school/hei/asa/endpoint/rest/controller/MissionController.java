67becfd04499ca4aa06a6346077788e612c833d3
Naitive13 <hei.titan.2@gmail.com>
2025-10-03 16:40:17 +0300

    refactor: separate exportToCSV into mulptiple functions

--- src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
@@ -3,17 +3,13 @@ package school.hei.asa.endpoint.rest.controller;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.time.ZoneId;
 import java.util.*;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -102,82 +98,20 @@ public class MissionController {
     return "missions";
   }
 
+  @SneakyThrows
   @GetMapping("/missions/export-to-csv")
   public ResponseEntity<ByteArrayResource> exportToCSV(@RequestParam String workerCode) {
-    var totalWorkDaysPerWorker = missionService.totalWorkDaysPerWorker(workerCode);
-    String filePath = System.getProperty("java.io.tmpdir");
-    String fileName =
-        workerCode == null || workerCode.isBlank()
-            ? "total_work_days-All.csv"
-            : "total_work_days-"
-                + totalWorkDaysPerWorker.keySet().stream().findFirst().get()
-                + ".csv";
-    File file = new File(filePath, fileName);
-    try (FileWriter fileWriter = new FileWriter(file)) {
-      fileWriter.write(
-          "code,worker,worker level,start date,"
-              + "contract duration (in days),"
-              + "total days worked,remaining days"
-              + System.lineSeparator());
-      fileWriter.flush();
-      totalWorkDaysPerWorker.forEach(
-          (worker, thWorkerLevelHistories) -> {
-            thWorkerLevelHistories.parallelStream()
-                .forEach(
-                    thWorkerLevelHistory -> {
-                      try {
-                        String remainingDays;
-                        if (thWorkerLevelHistory.projectedDaysToWork().equals("-")) {
-                          remainingDays = "-";
-                        } else {
-                          var number =
-                              (Double.parseDouble(thWorkerLevelHistory.projectedDaysToWork())
-                                  - Double.parseDouble(thWorkerLevelHistory.actualWorkedDay()));
-                          var numberFormat = new DecimalFormat("#.0");
-                          remainingDays = numberFormat.format(number);
-                        }
-                        var actualWorkedDays =
-                            Double.parseDouble(thWorkerLevelHistory.actualWorkedDay()) == 0.0d
-                                ? "-"
-                                : thWorkerLevelHistory.actualWorkedDay();
-                        fileWriter.write(
-                            worker.code()
-                                + ","
-                                + worker.name()
-                                + ","
-                                + thWorkerLevelHistory.level()
-                                + ","
-                                + thWorkerLevelHistory
-                                    .entranceInstant()
-                                    .atZone(ZoneId.of("UTC"))
-                                    .toLocalDate()
-                                + ","
-                                + thWorkerLevelHistory.projectedDaysToWork()
-                                + ","
-                                + actualWorkedDays
-                                + ","
-                                + remainingDays
-                                + System.lineSeparator());
-                        fileWriter.flush();
-                      } catch (IOException e) {
-                        throw new RuntimeException(e);
-                      }
-                    });
-          });
-      ByteArrayResource resource =
-          new ByteArrayResource(Files.readAllBytes(Path.of(file.getAbsolutePath())));
-      HttpHeaders header = new HttpHeaders();
-      header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+    var file = missionService.generateCSV(workerCode);
+    ByteArrayResource resource =
+        new ByteArrayResource(Files.readAllBytes(Path.of(file.getAbsolutePath())));
+    HttpHeaders header = new HttpHeaders();
+    header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
 
-      return ResponseEntity.ok()
-          .headers(header)
-          .contentLength(file.length())
-          .contentType(MediaType.parseMediaType("application/octet-stream"))
-          .body(resource);
-
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return ResponseEntity.ok()
+        .headers(header)
+        .contentLength(file.length())
+        .contentType(MediaType.parseMediaType("application/octet-stream"))
+        .body(resource);
   }
 
   @GetMapping("/mission-executions")
--- src/main/java/school/hei/asa/service/MissionService.java
+++ src/main/java/school/hei/asa/service/MissionService.java
@@ -3,11 +3,23 @@ package school.hei.asa.service;
 import static java.util.Comparator.comparing;
 import static java.util.Comparator.naturalOrder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.hei.asa.endpoint.rest.controller.mapper.ThProductMapper;
@@ -222,5 +234,85 @@ public class MissionService {
       result.put(worker, workerLevelHistories);
     }
     return result;
+  }
+
+  public File generateCSV(String workerCode) {
+    var totalWorkDaysPerWorker = totalWorkDaysPerWorker(workerCode);
+    String filePath = System.getProperty("java.io.tmpdir");
+    String fileName =
+        workerCode == null || workerCode.isBlank()
+            ? "total_work_days-All.csv"
+            : "total_work_days-"
+                + totalWorkDaysPerWorker.keySet().stream().findFirst().get()
+                + ".csv";
+    File file = new File(filePath, fileName);
+    writeToFile(file, totalWorkDaysPerWorker);
+    return file;
+  }
+
+  @SneakyThrows
+  private void writeToFile(
+      File file, Map<Worker, List<ThWorkerLevelHistory>> totalWorkDaysPerWorker) {
+    try (FileWriter fileWriter = new FileWriter(file)) {
+      fileWriter.write(
+          "code,worker,worker level,start date,"
+              + "contract duration (in days),"
+              + "total days worked,remaining days"
+              + System.lineSeparator());
+      fileWriter.flush();
+      totalWorkDaysPerWorker.forEach(
+          (worker, thWorkerLevelHistories) -> {
+            thWorkerLevelHistories.parallelStream()
+                .forEach(
+                    thWorkerLevelHistory -> {
+                      try {
+                        String remainingDays = remainingDaysToString(thWorkerLevelHistory);
+                        String actualWorkedDays = actualWorkedDaysToString(thWorkerLevelHistory);
+                        String startDate =
+                            thWorkerLevelHistory
+                                .entranceInstant()
+                                .atZone(ZoneId.of("UTC"))
+                                .toLocalDate()
+                                .toString();
+                        String textToWrite =
+                            String.format(
+                                "%s,%s,%s,%s,%s,%s,%s",
+                                worker.code(),
+                                worker.name(),
+                                thWorkerLevelHistory.level(),
+                                startDate,
+                                thWorkerLevelHistory.projectedDaysToWork(),
+                                actualWorkedDays,
+                                remainingDays);
+
+                        fileWriter.write(textToWrite + System.lineSeparator());
+                        fileWriter.flush();
+                      } catch (IOException e) {
+                        throw new RuntimeException(e);
+                      }
+                    });
+          });
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String remainingDaysToString(ThWorkerLevelHistory thWorkerLevelHistory) {
+    if (thWorkerLevelHistory.projectedDaysToWork().equals("-")) {
+      return "-";
+    } else {
+      var number =
+          (Double.parseDouble(thWorkerLevelHistory.projectedDaysToWork())
+              - Double.parseDouble(thWorkerLevelHistory.actualWorkedDay()));
+      var numberFormat = new DecimalFormat("#.0");
+      return numberFormat.format(number);
+    }
+  }
+
+  private String actualWorkedDaysToString(ThWorkerLevelHistory thWorkerLevelHistory) {
+    return Double.parseDouble(thWorkerLevelHistory.actualWorkedDay()) == 0.0d
+        ? "-"
+        : thWorkerLevelHistory.actualWorkedDay();
   }
 }

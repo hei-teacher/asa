d1e7e3743a0320273c7a91fb76a95839f22af6f4
Naitive13 <hei.titan.2@gmail.com>
2026-01-14 14:34:20 +0300

    test: add new data after migration

--- src/test/resources/db/migration/V101_1__Insert_contract_levels.sql
+++ src/test/resources/db/migration/V101_1__Insert_contract_levels.sql
@@ -1,5 +1,6 @@
 INSERT INTO contract_level
- (level_id, code, type, daily_pay)
-VALUES ('useless-id-to-rm', 'L5P', 'partnerContractor', 50000);
+ (code, type, daily_pay)
+VALUES ( 'L5P-2026', 'partnerContractor', 50000),
+       ( 'L4P-2026', 'partnerContractor', 100000);
 
 UPDATE contract_level SET type = 'studentContractor', daily_pay = 50000 WHERE code = 'L5';

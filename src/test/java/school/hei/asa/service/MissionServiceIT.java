3370d1098d41e0799b4dabd9df0d6928b6dd75c1
Naitive13 <hei.titan.2@gmail.com>
2026-01-22 15:51:10 +0300

    fix: remove extra code after rebasing preprod

--- src/test/java/school/hei/asa/service/MissionServiceIT.java
+++ src/test/java/school/hei/asa/service/MissionServiceIT.java
@@ -3,7 +3,6 @@ package school.hei.asa.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import school.hei.asa.conf.FacadeIT;
 
-@Transactional
 class MissionServiceIT extends FacadeIT {
 
   @Autowired MissionService missionService;

e57b100de9381d3415e6b3f5ae97ef429dfcd8b4
Naitive13 <hei.titan.2@gmail.com>
2026-01-29 15:57:06 +0300

    fix(daily-executions): map missions without missionExecutions

--- src/main/java/school/hei/asa/service/MissionService.java
+++ src/main/java/school/hei/asa/service/MissionService.java
@@ -1,10 +1,19 @@
 package school.hei.asa.service;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import school.hei.asa.model.Mission;
+import school.hei.asa.repository.MissionRepository;
 
 @Slf4j
 @AllArgsConstructor
 @Service
-public class MissionService {}
+public class MissionService {
+  private final MissionRepository missionRepository;
+
+  public List<Mission> getAllMissions() {
+    return missionRepository.findAll();
+  }
+}

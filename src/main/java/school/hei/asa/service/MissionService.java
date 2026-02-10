67d8017eca72dced6453e6ea07a6acfa052d1c71
Naitive13 <hei.titan.2@gmail.com>
2026-02-10 16:10:36 +0300

    feat: delete useless log

--- src/main/java/school/hei/asa/service/MissionService.java
+++ src/main/java/school/hei/asa/service/MissionService.java
@@ -24,8 +24,6 @@ public class MissionService {
 
   public boolean isUnpaidCare(MissionExecution me) {
     var mission = me.mission();
-    log.info("here is the care product : {}", careProductCodeSupplier.get());
-    log.info("here are the paid care mission : {}", paidCareMissionCodesSupplier.get());
     return mission.isCare(careProductCodeSupplier.get())
         && !mission.isPaidCare(paidCareMissionCodesSupplier.get());
   }

60be6d679f69c89eb8757183349b15da3d05b387
SalomiaZK <salomia.razakarivony@gmail.com>
2026-02-27 14:29:16 +0300

    test: get_by_date_between in an isolated class

--- src/test/java/school/hei/asa/endpoint/rest/service/ThProductServiceIT.java
+++ src/test/java/school/hei/asa/endpoint/rest/service/ThProductServiceIT.java
@@ -127,46 +127,4 @@ class ThProductServiceIT extends FacadeIT {
     assertEquals(0, result.getFirst().missions().getFirst().getMissionExecutions().size());
     assertEquals(1, result.getLast().missions().getFirst().getMissionExecutions().size());
   }
-
-  @Test
-  void filter_product_by_date_between() {
-    var missionExecution1 =
-        new ThMissionExecution(
-            "mission0-code",
-            "W-P-2024-01",
-            LocalDate.parse("2024-07-01"),
-            0.5,
-            "comment3",
-            false,
-            true);
-    var missionExecution2 =
-        new ThMissionExecution(
-            "mission0-code",
-            "W-P-2024-01",
-            LocalDate.parse("2024-07-01"),
-            0.5,
-            "comment4",
-            false,
-            true);
-    var mission =
-        new ThMission(
-            "mission0-code",
-            "a mission",
-            "a description",
-            List.of(missionExecution1, missionExecution2),
-            false,
-            false);
-    var expected = new ThProduct("pCode0", "pname0", "pDescription0", List.of(mission), false);
-
-    var actual =
-        thProductService
-            .filterThProductByWorkerCodeAndDateBetween(
-                "W-P-2024-01", "2024-06-01", "2024-08-01", true)
-            .stream()
-            .filter(p -> p.code().equals("pCode0"))
-            .findFirst()
-            .orElseThrow(() -> new AssertionError("Test product not found"));
-
-    assertEquals(expected, actual);
-  }
 }

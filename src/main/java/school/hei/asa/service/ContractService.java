e8fab259f5aa70bd13ed8fdbf156df4087807a69
SalomiaZK <salomia.razakarivony@gmail.com>
2026-04-29 22:00:26 +0300

    chore: format

--- src/main/java/school/hei/asa/service/ContractService.java
+++ src/main/java/school/hei/asa/service/ContractService.java
@@ -34,6 +34,7 @@ public class ContractService {
   private final DateTimeFormatter localDateFormatter =
       DateTimeFormatter.ofPattern("dd MMM yyyy", FRENCH);
 
+
   public Map<Worker, List<Contract>> totalWorkDaysPerWorker() {
     return contractRepository.findAll().stream().collect(Collectors.groupingBy(Contract::worker));
   }

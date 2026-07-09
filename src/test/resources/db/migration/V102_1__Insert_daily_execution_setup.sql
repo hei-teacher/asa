f88e97d985125b20018466579d627e8d81f3b4b3
Manda Tiavina <solontianamandanirina@gmail.com>
2026-07-09 14:15:59 +0300

    refactor: move inline SQL inserts in DailyExecutionControllerIT to V102 migration files

--- src/test/resources/db/migration/V102_1__Insert_daily_execution_setup.sql
+++ src/test/resources/db/migration/V102_1__Insert_daily_execution_setup.sql
@@ -1,11 +1,10 @@
-INSERT INTO worker (code, name, email, fullname, address, city, nif, stat)
-VALUES ('worker-code', 'code', 'email', 'full code', 'address', 'random city', 'nif', 'stat')
-ON CONFLICT DO NOTHING;
+insert into worker (code, name)
+values ('worker-code', 'code');
 
-INSERT INTO contract_level (code, type, daily_pay)
-VALUES ('L-TEST', 'studentContractor', 25000)
-ON CONFLICT DO NOTHING;
+INSERT INTO contract_level
+ (code, type, daily_pay)
+VALUES ('L-TEST', 'studentContractor', 25000);
 
-INSERT INTO contract (id, worker_code, level, entrance_instant, duration_in_days)
-VALUES ('contract-daily-exec', 'worker-code', 'L-TEST', '2025-01-01 00:00:00'::timestamp, 80)
-ON CONFLICT DO NOTHING;
+INSERT INTO contract
+    (id,worker_code, level, entrance_instant, job_title, duration_in_days, contract_bucket_key)
+VALUES ('contract-daily-exec','worker-code','L-TEST','2025-01-01 00:00:00.000000','job_title', 80, 'contract_bucket_key');

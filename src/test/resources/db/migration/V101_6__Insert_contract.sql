cf79a5fbed94a21655c1f566facff87a5d9c2357
SalomiaZK <salomia.razakarivony@gmail.com>
2026-08-21 20:31:51 +0300

    test: update contracts

---
+++ src/test/resources/db/migration/V101_6__Insert_contract.sql
@@ -0,0 +1,3 @@
+INSERT INTO contract
+(id,worker_code, level, entrance_instant, job_title, duration_in_days, contract_bucket_key)
+VALUES ('new-id','worker-code','L4P-2026','2025-01-01 08:00:00.000000','job_title', 80, 'contract_bucket_key');

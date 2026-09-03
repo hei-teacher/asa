f0bd24ed3adf233410be7dc5fe39c2a3b3a98530
SalomiaZK <salomia.razakarivony@gmail.com>
2026-09-03 22:48:06 +0300

    feat: create Tax models

--- src/main/java/school/hei/asa/number/NullToBigDecimalHanlder.java
+++ src/main/java/school/hei/asa/number/NullToBigDecimalHanlder.java
@@ -1,6 +1,7 @@
 package school.hei.asa.number;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class NullToBigDecimalHanlder {
   public static BigDecimal toBigDecimalOrZero(Double value) {
@@ -9,5 +10,10 @@ public class NullToBigDecimalHanlder {
 
   public static double toDoubleOrZero(Double value) {
     return value == null ? 0 : value;
+  }
+
+  public static BigDecimal calculatePercentageValue(Double percentage, BigDecimal base) {
+    return base.multiply(toBigDecimalOrZero(percentage))
+        .divide(new BigDecimal(100), 2, RoundingMode.HALF_UP);
   }
 }
